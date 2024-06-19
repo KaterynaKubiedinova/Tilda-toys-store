c32cd83ff0809e0679cd953771f367b59c12463a
Kateryna Kubiedinova <kkubiedinova@gmail.com>
2024-06-19 15:00:25 +0200

    added header  to axios

--- src/app/tools/api.ts
+++ src/app/tools/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.API_HOST
+  baseURL: process.env.API_HOST,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  }
 });
 
 export default api;
